d6b13c7a00baf6fe3c52cdc18ae8d5d740d77c42
Thomas Alex <thomas.a@tiflabs.in>
2021-09-02 23:01:50 +0530

    - Connected fun translator API - Js part is fully functional

--- app.js
+++ app.js
@@ -3,16 +3,32 @@ var click_btn = document.querySelector("#click-btn");
 var text_area = document.querySelector("#txt-area");
 var output_box = document.querySelector("#output-box");
 
+var url = "https://api.funtranslations.com/translate/minion.json"
+
 function log(msg) {
     console.log(msg);
 };
+
+function urlConstructor(msg) {
+    return url + "?text=" + msg;
+}
+
+function errorHandler(error) {
+    log("An error has occured")
+    alert("Server is down. Please try again later")
+}
 
 log(text_area)
 
 function buttonClickFunc() {
     log("Clicked");
-    log(text_area.value);
-    output_box.innerText = text_area.value
+    fetch(urlConstructor(text_area.value))
+        .then(response => response.json())
+        .then(json => {
+            var translated = json.contents.translated
+            output_box.innerText = translated
+        })
+    .catch(errorHandler)
 };
 
 click_btn.addEventListener("click", buttonClickFunc);
